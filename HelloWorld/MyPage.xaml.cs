df57c9771367218e21cfac1b30c555c94d530036
Worameth Semapat <worameth.semapat@gmail.com>
2018-05-29 14:49:36 +0700

    first init

---
+++ HelloWorld/MyPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MyPage : ContentPage
+    {
+        public string Name { get; set; } = "Helloz";
+        public MyPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        async void Button_OnClicked(object sender, System.EventArgs e)
+        {
+            string text = MainEntry.Text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                Name = "Hello Null";
+            }
+            else
+            {
+                Name = $"Hello {text}";
+            }
+            await DisplayAlert("Title", Name, "Ok");
+        }
+
+        async void Button_GoButtonScreen(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new MyPage3());
+        }
+
+        void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            MainLabel.Text = MainEntry.Text;
+        }
+    }
+}
